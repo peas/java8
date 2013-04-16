4d0097054f2b35b057d63831744cded38021a3c7
Paulo Silveira <paulo.silveira@caelum.com.br>
2013-04-16 17:07:11 -0300

    inicio

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 java8
 =====
+
+Se quer executar os testes via maven, você precisa ajustar a variável de ambiente JAVA_HOME_8 para apontar para a home do seu JDK8, ou então remover essa configuração de dentro do pom.xml, no caso de já ser seu default.
+
---
+++ src/test/java/br/com/casadocodigo/java8/BuscaAlunoTest.java
@@ -0,0 +1,20 @@
+package br.com.casadocodigo.java8;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BuscaAlunoTest 
+    extends TestCase
+{
+
+    @Test
+    public void testApp()
+    {
+        Runnable r = null;
+        assertTrue( true );
+    }
+}
