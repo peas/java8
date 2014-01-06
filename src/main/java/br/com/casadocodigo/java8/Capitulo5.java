3c78bec5774a69a4d0850a859938754c2cb85f8b
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2014-01-06 17:45:26 -0200

    removendo comentarios e TODOs já tratados

--- src/main/java/br/com/casadocodigo/java8/Capitulo5.java
+++ src/main/java/br/com/casadocodigo/java8/Capitulo5.java
@@ -26,10 +26,6 @@ class Capitulo5 {
 
 		usuarios.sort(comparator);
 
-		
-		// TODO: corrigir o texto (talvez explicar o porque...)
-		// não funciona com (u1, u2) por causa do escopo
-		
 		Comparator<Usuario> comparatorComLambda = 
 			  (u1, u2) -> u1.getNome().compareTo(u2.getNome());
 
@@ -40,56 +36,26 @@ class Capitulo5 {
 		
 		usuarios.sort((u1, u2) -> u1.getNome().compareTo(u2.getNome()));
 		
-		// TODO: porque não compila sem o <Usuario, String> ?
-		// na spec fala que ele infere o primeiro argumento do tipo Comparator<Usuario>
-		// e o segundo argumento do retorno do método u.getNome()
-		
 		Comparator<Usuario> comparator22 =
 				Comparator.comparing(Usuario::getNome)
 					.thenComparing(Usuario::getNome);
 
 		usuarios.sort(Comparator.comparing(u -> u.getPontos()));
 
-
-		//usuarios.sort(Comparator.comparing(u -> u.getNome())
-		//			.thenComparing(u -> u.getPontos()));
-
-
-
-
 		List<String> list = Collections.emptyList();
-		list.sort(
-		        Comparator.comparing((String s)->s.toString())
+		list.sort(Comparator.comparing((String s)->s.toString())
         			.thenComparing(s -> s.length()));
 
-
 		Function<Usuario, Integer> extraiPontos = u -> u.getPontos();
-		Comparator<Usuario> comparator3 =
-			Comparator.comparing(extraiPontos);
+		Comparator<Usuario> comparator3 = Comparator.comparing(extraiPontos);
 		usuarios.sort(comparator3);
 
 
 		ToIntFunction<Usuario> extraiPontos2 = u -> u.getPontos();
-		Comparator<Usuario> comparator4 =
-			Comparator.comparingInt(extraiPontos2);
+		Comparator<Usuario> comparator4 = Comparator.comparingInt(extraiPontos2);
 		usuarios.sort(comparator4);
 
 		usuarios.sort(Comparator.comparingInt(u -> u.getPontos()));
-
-	//	usuarios.sort(Comparator.<String>comparing((String s) -> s.toString())
-	//								.thenComparing((String s) -> s.length()));
-
-
-		//usuarios.sort(Comparator.comparing(String::toString)
-		//							.thenComparing(String::length));
-
-
-
-
-
-//		Comparator<Usuario> c = Comparator.comparing(u -> u.toString())
-//									.thenComparing(u -> u.toString());
-
 
 	}
 }
