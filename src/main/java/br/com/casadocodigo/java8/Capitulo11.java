47b71c820f9f8b53f11267a0791026b31844180a
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2014-03-13 15:39:06 -0300

    adicionando alguns valores pra teste

--- src/main/java/br/com/casadocodigo/java8/Capitulo11.java
+++ src/main/java/br/com/casadocodigo/java8/Capitulo11.java
@@ -1,12 +1,16 @@
 package br.com.casadocodigo.java8;
 
-import java.util.*;
-import java.math.*;
-import java.time.*;
-import java.io.*;
-import java.nio.file.*;
-import java.util.stream.*;
-import java.util.function.*;
+import static java.util.Arrays.asList;
+
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 enum Type {
 	MUSIC, 
@@ -92,7 +96,28 @@ public class Capitulo11 {
 
 	public static void main (String... args) throws Exception 	{
 
-		List<Payment> payments = null;
+		Customer paulo = new Customer("Paulo Silveira");
+		Customer rodrigo = new Customer("Rodrigo Turini");
+		Customer guilherme = new Customer("Guilherme Silveira");
+		Customer adriano = new Customer("Adriano Almeida");
+		
+		Product p1 = new Product("P1", Paths.get(""), new BigDecimal(100));
+		Product p2 = new Product("P2", Paths.get(""), new BigDecimal(89.90));
+		Product p3 = new Product("P3", Paths.get(""), new BigDecimal(50));
+		Product p4 = new Product("P4", Paths.get(""), new BigDecimal(150.99));
+		Product p5 = new Product("P5", Paths.get(""), new BigDecimal(200));
+		Product p6 = new Product("P6", Paths.get(""), new BigDecimal(180));
+		
+		LocalDateTime today = LocalDateTime.now();
+		LocalDateTime yesterday = today.minusDays(1);
+		LocalDateTime tomorrow = today.plusDays(1);
+		
+		Payment payment1 = new Payment(asList(p1, p2), today, paulo);
+		Payment payment2 = new Payment(asList(p3, p1, p6), yesterday, rodrigo);
+		Payment payment4 = new Payment(asList(p6, p1, p2), tomorrow, guilherme);
+		Payment payment3 = new Payment(asList(p4, p5, p1), today, adriano);
+		
+		List<Payment> payments = asList(payment1, payment2, payment3, payment4);
 		// ordenar List<Payment> por data
 
 		payments.stream()
@@ -109,7 +134,7 @@ public class Capitulo11 {
 			.reduce(BigDecimal::add);			
 
 		// qual é o produto mais vendido?
-
+		
 		// qual é o usuario que mais pagou
 
 		// que tipo de produto é o mais vendido (sum(VIDEO), sum(AUDIO))
